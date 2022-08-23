b357e108e27231459e567b9d633542704557c74c
amigin <amigin@gmail.com>
2022-08-23 13:58:56 +0300

    Closed Crypto-deposit-operation behind feature

--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -1,4 +1,6 @@
+#[cfg(feature = "crypto-deposit-operation")]
 mod crypto_deposit_operation;
 mod swap;
+#[cfg(feature = "crypto-deposit-operation")]
 pub use crypto_deposit_operation::*;
 pub use swap::*;
