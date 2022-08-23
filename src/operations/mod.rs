3ceb20b81efe487fde08f542257b937d8612fdb1
amigin <amigin@gmail.com>
2022-08-23 14:44:42 +0300

    Swap-operations is behind feature

--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -1,6 +1,8 @@
 #[cfg(feature = "crypto-deposit-operation")]
 mod crypto_deposit_operation;
-mod swap;
+#[cfg(feature = "swap-operation")]
+mod swap_operation;
 #[cfg(feature = "crypto-deposit-operation")]
 pub use crypto_deposit_operation::*;
-pub use swap::*;
+#[cfg(feature = "swap-operation")]
+pub use swap_operation::*;
