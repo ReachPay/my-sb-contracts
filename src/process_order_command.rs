1d2fc3703ee2c07b3854d08baa484bc226475fee
amigin <amigin@gmail.com>
2022-08-27 22:46:17 +0300

    Renamed topic

--- src/process_order_command.rs
+++ src/process_order_command.rs
@@ -1,4 +1,4 @@
-pub static PROCESS_ORDER_COMMAND_TOPIC_NAME: &'static str = "charge-order-command";
+pub static PROCESS_ORDER_COMMAND_TOPIC_NAME: &'static str = "process-order-command";
 
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ProcessOrderComand {
