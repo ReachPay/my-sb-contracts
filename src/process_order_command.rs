7b84862ba7078eee4d08c89d9adfee615c81b44a
amigin <amigin@gmail.com>
2022-08-27 23:30:29 +0300

    Put traits to ProcessOrderComand

--- src/process_order_command.rs
+++ src/process_order_command.rs
@@ -1,7 +1,23 @@
+use crate::utils::*;
+
 pub static PROCESS_ORDER_COMMAND_TOPIC_NAME: &'static str = "process-order-command";
 
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ProcessOrderComand {
     #[prost(string, tag = "1")]
     pub order_id: String,
+}
+
+impl AsBytes for ProcessOrderComand {
+    fn as_bytes(&self) -> Vec<u8> {
+        let mut result = Vec::new();
+        prost::Message::encode(self, &mut result).unwrap();
+        result
+    }
+}
+
+impl FromBytes for ProcessOrderComand {
+    fn from_bytes(src: &[u8]) -> Self {
+        prost::Message::decode(src).unwrap()
+    }
 }
