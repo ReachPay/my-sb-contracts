ea562425b671739d4ae5adb62811ff301e96a620
amigin <amigin@gmail.com>
2022-10-12 00:49:34 +0300

    CryptoDepositOperation new supports merchant

--- src/crypto_deposit_operation.rs
+++ src/crypto_deposit_operation.rs
@@ -14,17 +14,19 @@ pub struct CryptoDepositOperation {
     #[prost(string, tag = "2")]
     pub process_id: String,
     #[prost(string, tag = "3")]
-    pub client_id: String,
+    pub client_id: Option<String>,
     #[prost(string, tag = "4")]
+    pub merchant_id: Option<String>,
+    #[prost(string, tag = "5")]
     pub asset_id: String,
-    #[prost(double, tag = "5")]
-    pub amount: f64,
     #[prost(double, tag = "6")]
+    pub amount: f64,
+    #[prost(double, tag = "7")]
     pub balance_after_operation: f64,
-    #[prost(string, tag = "7")]
-    pub transaction_id: String,
     #[prost(string, tag = "8")]
+    pub transaction_id: String,
+    #[prost(string, tag = "9")]
     pub deposit_address: String,
-    #[prost(message, tag = "9")]
+    #[prost(message, tag = "10")]
     pub commission: Option<CryptoDepositCommission>,
 }
